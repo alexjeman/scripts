2a6fc095e9c87ddc94e757651609d2e32db46885
alexjeman <alexandrujeman.ja@gmail.com>
2020-02-07 23:15:38 +0200

    Move to another repo

--- file_objects/read_file.py
+++
@@ -1,9 +0,0 @@
-import os
-
-dir_path = os.path.dirname(os.path.realpath(__file__))
-os.chdir(dir_path)
-
-with open('text_file.txt') as F:
-    content = F.read()
-    print(content)
-    print(content[:100])
